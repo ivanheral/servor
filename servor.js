4ccd5a0f7bd4d153c9780925db26751f22827aca
ivanheral <ivanhernandezal@gmail.com>
2019-04-26 17:00:13 +0200

    refactor servor

--- servor.js
+++ servor.js
@@ -15,26 +15,7 @@ const mime = Object.entries(require("./types.json")).reduce(
   {}
 );
 
-// ----------------------------------
-// Parse arguments from the command line
-// ----------------------------------
-
-const root = process.argv[2] || ".";
-const fallback = process.argv[3] || "index.html";
-const port = process.argv[4] || 8080;
-const reloadPort = process.argv[5] || 5000;
 const cwd = process.cwd();
-
-// ----------------------------------
-// Template clientside reload script
-// ----------------------------------
-
-const reloadScript = `
-  <script>
-    const source = new EventSource('http://localhost:${reloadPort}');
-    source.onmessage = e => location.reload(true);
-  </script>
-`;
 
 // ----------------------------------
 // Server utility functions
@@ -73,59 +54,68 @@ const isRouteRequest = uri =>
 // Start file watching server
 // ----------------------------------
 
-http
-  .createServer((request, res) => {
-    // Open the event stream for live reload
-    res.writeHead(200, {
-      Connection: "keep-alive",
-      "Content-Type": "text/event-stream",
-      "Cache-Control": "no-cache",
-      "Access-Control-Allow-Origin": "*"
-    });
-    // Send an initial ack event to stop request pending
-    sendMessage(res, "connected", "awaiting change");
-    // Send a ping event every minute to prevent console errors
-    setInterval(sendMessage, 60000, res, "ping", "still waiting");
-    // Watch the target directory for changes and trigger reload
-    fs.watch(path.join(cwd, root), { recursive: true }, () =>
-      sendMessage(res, "message", "reloading page")
-    );
-  })
-  .listen(parseInt(reloadPort, 10));
+const start = options => {
+  const root = options.root || ".";
+  const fallback = options.fallback || "index.html";
+  const port = options.port || 8080;
+  const reloadPort = options.reloadPort || 5000;
+
+// ----------------------------------
+// Template clientside reload script
+// ----------------------------------
+
+  const reloadScript = `
+  <script>
+    const source = new EventSource('http://localhost:${reloadPort}');
+    source.onmessage = e => location.reload(true);
+  </script>
+  `;
+
+  http
+    .createServer((request, res) => {
+      // Open the event stream for live reload
+      res.writeHead(200, {
+        Connection: "keep-alive",
+        "Content-Type": "text/event-stream",
+        "Cache-Control": "no-cache",
+        "Access-Control-Allow-Origin": "*"
+      });
+      // Send an initial ack event to stop request pending
+      sendMessage(res, "connected", "awaiting change");
+      // Send a ping event every minute to prevent console errors
+      setInterval(sendMessage, 60000, res, "ping", "still waiting");
+      // Watch the target directory for changes and trigger reload
+      fs.watch(path.join(cwd, root), { recursive: true }, () =>
+        sendMessage(res, "message", "reloading page")
+      );
+    })
+    .listen(parseInt(reloadPort, 10));
 
 // ----------------------------------
 // Start static file server
 // ----------------------------------
 
-http
-  .createServer((req, res) => {
-    const pathname = url.parse(req.url).pathname;
-    const isRoute = isRouteRequest(pathname);
-    const status = isRoute && pathname !== "/" ? 301 : 200;
-    const resource = isRoute ? `/${fallback}` : decodeURI(pathname);
-    const uri = path.join(cwd, root, resource);
-    const ext = uri.replace(/^.*[\.\/\\]/, "").toLowerCase();
-    isRoute && console.log("\n \x1b[44m", "RELOADING", "\x1b[0m\n");
-    // Check if files exists at the location
-    fs.stat(uri, (err, stat) => {
-      if (err) return sendError(res, resource, 404);
-      // Respond with the contents of the file
-      fs.readFile(uri, "binary", (err, file) => {
-        if (err) return sendError(res, resource, 500);
-        if (isRoute) file += reloadScript;
-        sendFile(res, resource, status, file, ext);
+  http
+    .createServer((req, res) => {
+      const pathname = url.parse(req.url).pathname;
+      const isRoute = isRouteRequest(pathname);
+      const status = isRoute && pathname !== "/" ? 301 : 200;
+      const resource = isRoute ? `/${fallback}` : decodeURI(pathname);
+      const uri = path.join(cwd, root, resource);
+      const ext = uri.replace(/^.*[\.\/\\]/, "").toLowerCase();
+      isRoute && console.log("\n \x1b[44m", "RELOADING", "\x1b[0m\n");
+      // Check if files exists at the location
+      fs.stat(uri, (err, stat) => {
+        if (err) return sendError(res, resource, 404);
+        // Respond with the contents of the file
+        fs.readFile(uri, "binary", (err, file) => {
+          if (err) return sendError(res, resource, 500);
+          if (isRoute) file += reloadScript;
+          sendFile(res, resource, status, file, ext);
+        });
       });
-    });
-  })
-  .listen(parseInt(port, 10));
-
-// ----------------------------------
-// Log startup details to terminal
-// ----------------------------------
-
-console.log(`\n 🗂  Serving files from ./${root} on http://localhost:${port}`);
-console.log(` 🖥  Using ${fallback} as the fallback for route requests`);
-console.log(` ♻️  Reloading the browser when files under ./${root} change`);
+    })
+    .listen(parseInt(port, 10));
 
 // ----------------------------------
 // Open the page in the default browser
@@ -139,4 +129,9 @@ const open =
       ? "start"
       : "xdg-open";
 
-require("child_process").exec(open + " " + page);
+  require("child_process").exec(open + " " + page);
+}
+
+module.exports = {
+  start: start
+}
